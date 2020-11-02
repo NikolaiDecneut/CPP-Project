a194b4940a3eddacce81da5685b3f3add50daa19
Nikolai Decneut <decneutn@rsd407.org>
2020-11-02 09:26:43 -0800

    Variable 1-10

---
+++ GS2-1 vari1-10/GS2-1 vari1-10.cpp
@@ -0,0 +1,15 @@
+// GS2-1 vari1-10.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	int number1 = 1;
+	printf("%i\n", number1);
+	number1++;
+	printf("%i\n", number1);
+
+	return 0;
+}
+
