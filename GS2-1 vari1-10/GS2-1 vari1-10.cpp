686fab11bf00f329a9bb924f52b59854e30fe148
Nikolai Decneut <decneutn@rsd407.org>
2020-11-02 16:35:07 -0800

    comments

--- GS2-1 vari1-10/GS2-1 vari1-10.cpp
+++ GS2-1 vari1-10/GS2-1 vari1-10.cpp
@@ -1,6 +1,12 @@
-// GS2-1 vari1-10.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+/*
+Name: Nikolai Decneut
+Problem: output 1-10 with 1 variable
+Pseudocode: use 1 variable
+Notes:  learning how to output multiple numbers with 1 variable
+Maintenance Log :
+Date:        Done:
+11/2/20    Printf statements and int variables
+*/
 #include <iostream>
 
 int main()
---
+++ GS2-2/GS2-2.cpp
@@ -0,0 +1,9 @@
+// GS2-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
