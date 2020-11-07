83b218c0a87b7f73fddbad3aae918612c500e998
Nikolai Decneut <decneutn@rsd407.org>
2020-11-07 12:33:16 -0800

    Fixed

--- CPP Project/CPP Project.cpp
+++ CPP Project/CPP Project.cpp
@@ -2,19 +2,74 @@
 //
 
 #include <iostream>
-
+#include <conio.h>
+#include <stdio.h>
 int main()
+
 {
-    std::cout << "Hello World!\n";
+	printf("Division problems.\n");
+	int i = 5;
+	int b = 2;
+	int r = 8;
+	int c;
+	float x = 6.3;
+	float y = 2.4;
+	float t = 4.8;
+	float z;
+// 6 division math problems
+	//int
+	c = i / b;
+	printf("a / b = % d\n", c);
+	z = x / y;
+	printf("x / y = % f\n", z);
+	c = x / y;
+	printf(" / y = % d\n", c);
+	z = i / b;
+	printf("a / b = % f\n", z);
+	c = i / y;
+	printf("a / y = % d\n", c);
+	z = x / b;
+	printf("x / b = % f\n", z);
+	//float
+	z = x / y;
+	printf("x / y = %f\n\n", z);
+	//mixed divide int by float into int (typecast)
+	c = i / y;
+	printf("i / y = %d\n\n", c);
+	// do modulus division of the same problem as above
+
+
+	printf("i / y = %d\n\n", c);
+	//mixed, divide float by an int into float (typecast)
+	z = t / b
+
+// 2 addition problems
+	printf("Addition problems. \n");
+	c = i + b;
+	printf("i + b = %d \n", c );
+	c = r + i;
+	printf("r + i = %d\n", c);
+
+// 2 subtraction problems
+	printf("Subtraction problems.\n");
+	c = i - b;
+	printf("i - b = %d \n", c);
+	c = r - b;
+	printf("r - b = %d\n\n", c);
+
+//2 multiplication problems
+	printf("Multiplication problems.\n");
+	c = i * b; 
+	printf("i * b = %d \n", c);
+	c = r * b;
+	printf("r * b = %d\n\n", c);
+
+	//this is a practice
+	//int answer = 125 / 100;
+	//printf("the answer is %d\n", answer);
+	//float newAnswer = 125.0 / 100.0;
+	//printf("float division answer is %f\n", newAnswer);
+	//int modAnswer = 125 % 100;
+	//printf("modulus is %d\n", modAnswer);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- GS2-2/GS2-2.cpp
+++ GS2-2/GS2-2.cpp
@@ -1,6 +1,6 @@
 /*
 Name:  Nikolai Decneut
-	Problem:  do some division problems using ints and floats
+	Problem:  some division problems using ints and floats
 	Pseudocode:  
 		Declare + activate variables
 		Do some math and output results to screen
@@ -12,19 +12,23 @@ Name:  Nikolai Decneut
 
 */
 
+#include <iostream>
 #include <conio.h>
 #include <stdio.h>
-
 int main()
+
 {
-	printf("Division problems.\n\n");
+	printf("Division problems.\n");
 	int i = 5;
 	int b = 2;
+	int r = 8;
 	int c;
 	float x = 6.3;
 	float y = 2.4;
+	float t = 4.8;
 	float z;
-
+	// 6 division math problems
+		//int
 	c = i / b;
 	printf("a / b = % d\n", c);
 	z = x / y;
@@ -34,20 +38,48 @@ int main()
 	z = i / b;
 	printf("a / b = % f\n", z);
 	c = i / y;
-	printf("a / y = % d\n", c);	
+	printf("a / y = % d\n", c);
 	z = x / b;
 	printf("x / b = % f\n", z);
-	c = i / (int)y;
-	printf("a / y = % d\n", c);
-	z = x / (float)b;
-	printf("x / b = % f\n", z);
+	//float
+	z = x / y;
+	printf("x / y = %f\n\n", z);
+	//mixed divide int by float into int (typecast)
+	c = i / y;
+	printf("i / y = %d\n\n", c);
+	// do modulus division of the same problem as above
 
-	c = i / b;
-	printf("a / b = % d\n", c);
-	int d = i % b;
-	printf("a % %b = % d\n", d);
 
-	printf("Thank you, Nikolai Decneut");
-/*I need to work on this i dont have it memorized at all!*/
-	return 0;
+	printf("i / y = %d\n\n", c);
+	//mixed, divide float by an int into float (typecast)
+	z = t / b
+
+	//2 addition problems
+	printf("Addition problems. \n");
+	c = i + b;
+	printf("i + b = %d \n", c);
+	c = r + i;
+	printf("r + i = %d\n", c);
+
+	// 2 subtraction problems
+	printf("Subtraction problems.\n");
+	c = i - b;
+	printf("i - b = %d \n", c);
+	c = r - b;
+	printf("r - b = %d\n\n", c);
+
+	//2 multiplication problems
+	printf("Multiplication problems.\n");
+	c = i * b;
+	printf("i * b = %d \n", c);
+	c = r * b;
+	printf("r * b = %d\n\n", c);
+
+	//this is a practice
+	//int answer = 125 / 100;
+	//printf("the answer is %d\n", answer);
+	//float newAnswer = 125.0 / 100.0;
+	//printf("float division answer is %f\n", newAnswer);
+	//int modAnswer = 125 % 100;
+	//printf("modulus is %d\n", modAnswer);
 }
