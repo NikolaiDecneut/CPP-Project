c85761d0551697f1098d3ee94a00db64901815b3
Mikhail Chalakov <mikhail@chalakov.net>
2021-01-04 21:28:45 -0800

    microwave go mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm

--- GS3-4/MyshisVersion/main.cpp
+++ GS3-4/MyshisVersion/main.cpp
@@ -41,6 +41,8 @@ int main(){
             default:
                 consolePrintMSG("Ok deleting save....", narrator.color);
                 remove("saves/save.txt");
+                pause(1000);
+                clearterm();
                 break;
         }
     }
@@ -61,7 +63,9 @@ int main(){
             pause(2000);
             clearterm();
             consolePrintMSG("Welcome to seattle A.K.A the lost city, here you will-", narrator.color);
+            pause(500);
             consolePrintMSG("HEY, private wake up! No sleeping on guard duty or ill report you to the master.", sergent.color);
+            pause(2000);
             consolePrintMSG("Possible responses: \n 1.Yes sir, sorry sir!\n 2.Screw off.\n 3.Don't say anything.\n", narrator.color);
             int select = get();
             switch(select){
@@ -89,20 +93,19 @@ int main(){
             switch(choice){
                 case 1:
                     choice *= 1;
-                    consolePrintMSG("Yeah that's what I thought magot", sergent.color);
                     break;
                 case 2:
                     choice *= 2;
                         consolePrintMSG("*you wake up 30 minutes later with a bad headache and notice that a patrol is heading you way for a checkin.*", narrator.color);
-                        pause(1000);
+                        pause(3000);
                         consolePrintMSG("Oh no, what am i gonna say when the patrol asks why i didnt checkin. Let me go and speak with them and try to get out of this.", player.color);
-                        pause(1000);
+                        pause(2000);
                         consolePrintMSG("*the patrol arrives at your position.*", narrator.color);
                         pause(1000);
                         consolePrintMSG("Hey private, why didnt you checkin with your report.", patrol.color);
-                        pause(1000);                            
+                        pause(2000);                            
                         consolePrintMSG("You now have a choice do you snitch[1] on the sergeant or lie[2] for him?", narrator.color);
-                        pause(1000);
+                        pause(2000);
                     break;
                 case 3:
                     choice *= 3;
@@ -127,11 +130,12 @@ int main(){
         }
         if(depth == 2)
         {
-            if(choice == 6 || choice == 9){
+            if(choice == 4 || choice == 9){
                 switch(get()){
                     case 1:
                         choice *= 1;
                         consolePrintMSG("Okay, I'll talk to the sergeant about messing with the guards. Thank you private.", patrol.color);
+                        pause(2000);
                         break;
                     case 2:
                         choice *= 2;
@@ -143,12 +147,12 @@ int main(){
             {
                 /* Stuff for yeah thats what i thought */
             }
-            ++depth;
+            player.updateLocation(++depth, choice);
         }
         if(depth == 3){
-            if(choice == 6 || choice == 9){
+            if(choice == 4 || choice == 9){
                 consolePrintMSG("Okay they are at a meeting now is a perfct time to run, do you?", narrator.color);
-                consolePrintMSG("1.Run\n 2.Stay at your post.\n", narrator.color);
+                consolePrintMSG(" 1.Run\n 2.Stay at your post.\n", narrator.color);
                 switch(get()){
                     case 1:
                         consolePrintMSG(action.find("shot")->second, narrator.color);
@@ -168,6 +172,7 @@ int main(){
             else{
                 gameOver();
             }
+            player.updateLocation(++depth, choice);
         }
 
     }
