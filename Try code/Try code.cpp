6d13e3844ba681e78c49b16a9a9748e6abcace12
Nikolai Decneut <decneutn@rsd407.org>
2021-01-04 22:22:54 -0800

    mmmmmmmmmmmmmmmm idek

--- Try code/Try code.cpp
+++ Try code/Try code.cpp
@@ -11,7 +11,48 @@
 		11/24/20	created a programming quiz using switches
 */
 
+#include <string>
+#include <system_error>
+#include <Color.Dlg>
 
+int main()
+{
+
+	char sergeantColor = 'r';
+	char playerColor = 'b';
+	char narratorColor = 'w';
+	int color = 0;
+
+	std::string colorMSG(std::string value, char color); {
+		 value; ;.append(RST);
+		switch (color)
+		{
+		case 'r':
+			value.insert(0, );
+			break;
+		case 'g':
+			value.insert(0, KGRN);
+			break;
+		case 'b':
+			value.insert(0, KBLU);
+			break;
+		case 'w':
+			value.insert(0, KWHT);
+			break;
+		case 'm':
+			value.insert(0, KMAG);
+			break;
+		case 'y':
+			value.insert(0, KYEL);
+			break;
+		case 'c':
+			value.insert(0, KCYN);
+			break;
+		default:
+			break;
+		}
+}
+/*
 #include <stdio.h>
 #include <iostream>
 #include <conio.h>
@@ -22,8 +63,11 @@
 #include <cstdio>
 #include <stdbool.h>
 
+
 int main()
 {
+
+	
 	printf(" Welcome to your own adventure where you choose what happens; tell me your name: \n");
 	std::string n;
 	std::cin >> n;
@@ -31,41 +75,39 @@ int main()
 	printf(" Hello %s\n", n.c_str());
 	printf(" %s in this story you will be choosing the path of the story.", n.c_str());
 	printf(" Based on your decision the character will die or live");
+	printf(" please tell me the name of the character in this story: \n");
+	std::string m;
+	std::cin >> m;
 	fseek(stdin, 0, SEEK_END);
 	printf("\nThank you %s.\n", n.c_str());
-	// the ints below are used for the switches
-	int j = 0;
-	int	i = 0;
-	int t = 0;
-	bool correct = false;
-	int story = 0;
 	printf(" Welcome to the main menu, pleae select your adventure.\n");
 	printf(" Option 1\n This takes place in a city ravaged by war from the perspective of a lower class participant in a crime cindicate where you must make vital decisions to be able to escape the city.\n");
 	printf(" Option 2\n This story puts you in the shoes of a swat team leader and you have to make the right choices if you want to make the bust.\n");
 	printf(" Option 3\n Here you are a PoW and if you dont make the right choices you'll be executed.\n");
-	while (!correct) {
-		printf("%s thats not the one i chose to code\n Option: ", n.c_str());
-		scanf_s("%d", &story);
-		fseek(stdin, 0, SEEK_END);
-		if (story == 1) {
-			correct = true;
-		}
-	}
+	int story = 0;
+	printf("%s please make your choice\n Option: ", n.c_str());
+	scanf_s("%d", &story);
+	fseek(stdin, 0, SEEK_END);
 	switch (story)
 	{
 	case 1:
 		printf("\n Great choice, cause its not like i only chose to code for this one.");
 		break;
+	case 2:
+		printf("\n sorry this isnt the one i chose to write the code on. Please try again.");
+		break;
+	case 3:
+		printf("\n sorry this isnt the one i chose to write the code on. Please try again.");
+		break;
 	default:
 		printf("\n You didnt even choose one of the options. ");
 		break;
 	}
+	// find a way to loop this until the right decision is made.
 	printf("\n\n Welcome to seattle here we live among the few that survived the missle strike, the survivers are in hiding or are serving criminal enterprises for food, shelter and safety. \n  ");
 	printf("Welcome to seattle A.K.A the lost city, here you will ; HEY, private wake up! No sleeping on guard duty or ill report you to the master.\n\n");
 	int noSleep = 0;
 	int beCareful = 0;
-	int run = 0;
-
 	printf(" What is your response to the sergeant.\n");
 	printf(" 1.Yes sir, sorry sir!\n 2.Screw off.\n 3.Don't say anything.\n");
 	scanf_s("%d", &noSleep);
@@ -76,7 +118,7 @@ int main()
 		printf(" Yea thats what i thought.");
 		//expand story
 		break;
-		
+
 	case 2:
 		printf("What did you say private?");
 		printf(" I said, screw off sergeant.");
@@ -107,7 +149,7 @@ int main()
 				printf(" Okay, ill talk to the sergeant about messing with the guards. Thank you private.\n");
 				printf(" *okay they are at a meeting now is a perfct time to run, do you?\n");
 				printf(" 1. run\n 2. stay at your post.\n");
-				
+				int run = 0;
 				scanf_s("%d", &run);
 				fseek(stdin, 0, SEEK_END);
 				switch (run)
@@ -119,14 +161,14 @@ int main()
 					printf("\n You didnt take the opportunity and they came back and took you to be executed.\n GAME OVER");
 					break;
 				}
-			break;
+				break;
 
 			case 2:
 				printf(" Thats it private your going to detention.");
-				break; 
+				break;
 			}
-			
-		
+
+
 		}
 
 
@@ -135,6 +177,4 @@ int main()
 	return 0;
 
 }
-
-
-
+*/
